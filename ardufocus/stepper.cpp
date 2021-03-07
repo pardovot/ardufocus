f8711e66d5e0aa9e7c21e4633610e022b0ce2019
pardovot <pardosea@gmail.com>
2021-03-07 04:11:55 +0200

    format

--- ardufocus/stepper.cpp
+++ ardufocus/stepper.cpp
@@ -19,16 +19,16 @@
 
 #include "stepper.h"
 
-/**
- * @brief [brief description]
- * @details [long description]
- *
- */
+ /**
+  * @brief [brief description]
+  * @details [long description]
+  *
+  */
 void stepper::init() {
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    m_speed = 2;
-    m_ovf_counter = 0;
-  }
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		m_speed = 2;
+		m_ovf_counter = 0;
+	}
 }
 
 /**
@@ -37,8 +37,8 @@ void stepper::init() {
  *
  */
 void stepper::move() {
-  m_ovf_counter = 0;
-  m_position.moving = true;
+	m_ovf_counter = 0;
+	m_position.moving = true;
 }
 
 /**
@@ -47,8 +47,8 @@ void stepper::move() {
  *
  */
 bool stepper::is_moving() {
-  const bool b = (m_position.moving);
-  return b;
+	const bool b = (m_position.moving);
+	return b;
 }
 
 /**
@@ -57,10 +57,10 @@ bool stepper::is_moving() {
  *
  */
 void stepper::halt() {
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    m_position.target = m_position.current;
-    m_position.moving = false;
-  }
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		m_position.target = m_position.current;
+		m_position.moving = false;
+	}
 }
 
 /**
@@ -69,8 +69,8 @@ void stepper::halt() {
  *
  */
 uint32_t stepper::get_current_position() {
-  const uint32_t c = m_position.current;
-  return c;
+	const uint32_t c = m_position.current;
+	return c;
 }
 
 /**
@@ -78,11 +78,11 @@ uint32_t stepper::get_current_position() {
  * @details [long description]
  *
  */
-void stepper::set_current_position(const uint32_t &target) {
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    m_position.current = target;
-    m_position.target = target;
-  }
+void stepper::set_current_position(const uint32_t& target) {
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		m_position.current = target;
+		m_position.target = target;
+	}
 }
 
 /**
@@ -91,8 +91,8 @@ void stepper::set_current_position(const uint32_t &target) {
  *
  */
 uint16_t stepper::get_speed() {
-  const uint16_t s = m_speed;
-  return s;
+	const uint16_t s = m_speed;
+	return s;
 }
 
 /**
@@ -100,8 +100,8 @@ uint16_t stepper::get_speed() {
  * @details [long description]
  *
  */
-void stepper::set_speed(const uint16_t &target) {
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) { m_speed = target; }
+void stepper::set_speed(const uint16_t& target) {
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) { m_speed = target; }
 }
 
 /**
@@ -110,8 +110,8 @@ void stepper::set_speed(const uint16_t &target) {
  *
  */
 uint32_t stepper::get_target_position() {
-  const uint32_t t = m_position.target;
-  return t;
+	const uint32_t t = m_position.target;
+	return t;
 }
 
 /**
@@ -119,34 +119,34 @@ uint32_t stepper::get_target_position() {
  * @details [long description]
  *
  */
-void stepper::set_target_position(const uint32_t &target) {
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    m_position.target = target;
+void stepper::set_target_position(const uint32_t& target) {
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		m_position.target = target;
 
 #ifdef HAS_ACCELERATION
-    m_position.relative = 0;
+		m_position.relative = 0;
 
-    m_position.distance = (m_position.current > target)
-                              ? m_position.current - target
-                              : target - m_position.current;
+		m_position.distance = (m_position.current > target)
+			? m_position.current - target
+			: target - m_position.current;
 
-    if (m_position.distance >= ACCEL_MIN_STEPS) {
+		if (m_position.distance >= ACCEL_MIN_STEPS) {
 #if defined(USE_LINEAR_ACCEL)
-      m_position.easein = m_position.distance >> 1;
-      m_position.easeout = m_position.easein;
+			m_position.easein = m_position.distance >> 1;
+			m_position.easeout = m_position.easein;
 
 #elif defined(USE_TRAPEZOID_ACCEL) || defined(USE_SMOOTHSTEP_ACCEL)
-      if (m_position.distance < (ACCEL_DURATION << 1)) {
-        m_position.easein = m_position.distance >> 1;
-        m_position.easeout = m_position.easein;
-      } else {
-        m_position.easein = ACCEL_DURATION;
-        m_position.easeout = m_position.distance - ACCEL_DURATION;
-      }
+			if (m_position.distance < (ACCEL_DURATION << 1)) {
+				m_position.easein = m_position.distance >> 1;
+				m_position.easeout = m_position.easein;
+			} else {
+				m_position.easein = ACCEL_DURATION;
+				m_position.easeout = m_position.distance - ACCEL_DURATION;
+			}
 #endif
-    }
+		}
 #endif
-  }
+	}
 }
 
 /**
@@ -155,44 +155,44 @@ void stepper::set_target_position(const uint32_t &target) {
  *
  */
 void stepper::tick() {
-  // Movement guard
-  if (!m_position.moving) {
-    sleep();
-    return;
-  }
+	// Movement guard
+	if (!m_position.moving) {
+		sleep();
+		return;
+	}
 
-  // Step frequency generator
-  if ((m_ovf_counter++) < (TIMER0_FREQ / (m_set_speed << 1)) - 1) {
-    return;
-  }
-  m_ovf_counter = 0;
+	// Step frequency generator
+	if ((m_ovf_counter++) < (TIMER0_FREQ / (m_set_speed << 1)) - 1) {
+		return;
+	}
+	m_ovf_counter = 0;
 
-  // Speed control
-  // We're not 100% moonlite compatible here: the PPS speed value you select
-  // on the driver acts as a divider of *_MAX_SPEED set on the config file.
-  static uint8_t counter = 0;
-  if ((counter++) % (m_speed >> 1) != 0) {
-    return;
-  }
+	// Speed control
+	// We're not 100% moonlite compatible here: the PPS speed value you select
+	// on the driver acts as a divider of *_MAX_SPEED set on the config file.
+	static uint8_t counter = 0;
+	if ((counter++) % (m_speed >> 1) != 0) {
+		return;
+	}
 
-  // Move outwards
-  if (m_position.target > m_position.current) {
-    if ((m_invert_direction) ? step_cw() : step_ccw()) {
-      update_position(1);
-    }
-  }
+	// Move outwards
+	if (m_position.target > m_position.current) {
+		if ((m_invert_direction) ? step_cw() : step_ccw()) {
+			update_position(1);
+		}
+	}
 
-  // Move inwards
-  else if (m_position.target < m_position.current) {
-    if ((m_invert_direction) ? step_ccw() : step_cw()) {
-      update_position(-1);
-    }
-  }
+	// Move inwards
+	else if (m_position.target < m_position.current) {
+		if ((m_invert_direction) ? step_ccw() : step_cw()) {
+			update_position(-1);
+		}
+	}
 
-  // Stop movement
-  else {
-    halt();
-  }
+	// Stop movement
+	else {
+		halt();
+	}
 }
 
 #ifdef HAS_ACCELERATION
@@ -202,33 +202,33 @@ void stepper::tick() {
  *
  */
 void stepper::update_freq() {
-  if (m_position.distance >= ACCEL_MIN_STEPS) {
+	if (m_position.distance >= ACCEL_MIN_STEPS) {
 #if defined(USE_LINEAR_ACCEL)
-    const float s = (m_position.relative <= m_position.easein)
-                        ? m_position.relative
-                        : m_position.distance - m_position.relative;
-    const float f = map(s, 0, m_position.easein, 0.0, 1.0);
+		const float s = (m_position.relative <= m_position.easein)
+			? m_position.relative
+			: m_position.distance - m_position.relative;
+		const float f = map(s, 0, m_position.easein, 0.0, 1.0);
 
 #elif defined(USE_TRAPEZOID_ACCEL)
-    const float s = (m_position.relative <= m_position.easein)
-                        ? m_position.relative
-                        : (m_position.relative >= m_position.easeout)
-                              ? m_position.distance - m_position.relative
-                              : ACCEL_DURATION;
-    const float f = map(s, 0, ACCEL_DURATION, 0.0, 1.0);
+		const float s = (m_position.relative <= m_position.easein)
+			? m_position.relative
+			: (m_position.relative >= m_position.easeout)
+			? m_position.distance - m_position.relative
+			: ACCEL_DURATION;
+		const float f = map(s, 0, ACCEL_DURATION, 0.0, 1.0);
 
 #elif defined(USE_SMOOTHSTEP_ACCEL)
-    const float f =
-        util::smootheststep(0, m_position.easein, m_position.relative) *
-        (1.0 - util::smootheststep(m_position.easeout, m_position.distance,
-                                   m_position.relative));
+		const float f =
+			util::smootheststep(0, m_position.easein, m_position.relative) *
+			(1.0 - util::smootheststep(m_position.easeout, m_position.distance,
+			 m_position.relative));
 #endif
 
-    m_set_speed = util::lerp(m_min_speed, m_max_speed, f);
+		m_set_speed = util::lerp(m_min_speed, m_max_speed, f);
 
-  } else {
-    m_set_speed = m_min_speed;
-  }
+	} else {
+		m_set_speed = m_min_speed;
+	}
 }
 #endif
 
@@ -237,13 +237,13 @@ void stepper::update_freq() {
  * @details [long description]
  *
  */
-void stepper::update_position(const int8_t &direction) {
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    m_position.current += direction; // Update the global position
+void stepper::update_position(const int8_t& direction) {
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		m_position.current += direction; // Update the global position
 
 #ifdef HAS_ACCELERATION
-    ++m_position.relative; // Update the relative position
-    update_freq();         // Update the stepping frequency
+		++m_position.relative; // Update the relative position
+		update_freq();         // Update the stepping frequency
 #endif
-  }
+	}
 }
